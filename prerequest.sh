f50e33de69d368ce1aa79a8fdba67ecf6e2c1938
jianqiao.ms <jianqiao.ms@foxmail.com>
2016-02-26 18:18:16 +0800

    if语句中执行命令方式修改

--- prerequest.sh
+++ prerequest.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo -e "\033[;31mInstalling openstack packages...\033[0m"
-if [ "yum list installed|grep centos-release-openstack-liberty|wc -l" = "0" ]
+if [ `yum list installed|grep centos-release-openstack-liberty|wc -l` -eq 0 ]
 then
   echo -e "\033[;31mALREADY INSTALLED\033[0m"
   yum install centos-release-openstack-liberty -y
 fi
 
-if [ "yum upgrade | grep kernel | wc -l" != "0" ]
+if [ `yum upgrade | grep kernel | wc -l` != "0" ]
 then
   echo -e "\033[;31mKERNEL UPGRADED!REBOOT TO ACTIVE NEW KERNEL\033[0m"
   echo -n "Reboot now?(Y/N)"
