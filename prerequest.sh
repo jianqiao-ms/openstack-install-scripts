0b3ca39c0c754f1a6eb59cc1f23266a0cfc3f953
jianqiao.ms <jianqiao.ms@foxmail.com>
2016-02-26 18:05:26 +0800

    repair bugs

--- prerequest.sh
+++ prerequest.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 echo -e "\033[;31mInstalling openstack packages...\033[0m"
-if [ "yum list installed|grep centos-release-openstack-liberty|wc -l" -eq 0 ]
+if [ "yum list installed|grep centos-release-openstack-liberty|wc -l" = "0" ]
 then
   echo -e "\033[;31mALREADY INSTALLED\033[0m"
   yum install centos-release-openstack-liberty -y
 fi
 
-if [ "yum upgrade && yum update | grep kernel | wc -l" -ne 0 ]
+if [ "yum upgrade && yum update | grep kernel | wc -l" != "0" ]
 then
   echo "\033[0mKERNEL UPGRADED!REBOOT TO ACTIVE NEW KERNEL\033[0m"
   echo -n "Reboot now?(Y/N)"
   read reboot
   if [ $reboot = "Y" -o $reboot = "y" ]
+  then
     reboot
   else
     echo "pleas reboot system before continue"
